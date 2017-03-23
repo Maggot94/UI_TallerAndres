ce6897cfb105ad799881639f65cbfc90f31bba1c
Usuario-PC\Usuario <andrescastropuerta@hotmail.com>
2017-03-22 20:45:36 -0500

    UI_Taller

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour {
+
+	private int indice;
+
+	private Inventario myManager;
+
+	public int Indice {
+		get {
+			return indice;
+		}
+		set {
+			indice = value;
+		}
+	}
+
+	public Inventario MyManager {
+		get {
+			return myManager;
+		}
+		set {
+			myManager = value;
+		}
+	}
+
+	public void ClickOnMe(){
+
+		myManager.SetItemImage (indice);
+		
+	}
+}
---
+++ Assets/Scripts/Inventario.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventario : MonoBehaviour {
+
+	public Database Db;
+	public Image item;
+	public GameObject button;
+
+	void Start (){
+
+
+
+		for (int i = 0; i < Db.items.Length; i++) 
+		
+		{
+
+			GameObject objTemp = Instantiate( button);
+			objTemp.GetComponent<Buttons> ().Indice = i;
+			objTemp.GetComponentInChildren<Text> ().text = Db.items [i].itemName;
+			objTemp.GetComponent<Buttons> ().MyManager = this;
+			objTemp.transform.SetParent(transform);
+
+		}
+
+	}
+
+	public void SetItemImage(int indice)
+	{
+
+		item.sprite = Db.items [indice].sprite;
+
+	}
+}
